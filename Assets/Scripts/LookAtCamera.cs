49104c787e5ef5926b6ca8cdf3dbd99924d7d49f
ash_kmn <kamensky@sheridancollege.ca>
2021-03-10 16:50:38 -0500

    add running and idle animations

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -6,6 +6,7 @@ public class LookAtCamera : MonoBehaviour
 {
 
     private Camera theCam;
+    private float old_rotation = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,8 @@ public class LookAtCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        old_rotation = transform.eulerAngles.z;
         transform.LookAt(theCam.transform);
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, old_rotation);
     }
 }
