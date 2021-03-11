55e3dfd1384e569852ce00f85e2dcb34137cc0a2
Alex Durocher <57152028+durochal@users.noreply.github.com>
2021-03-11 16:38:52 -0500

    Music Box
    Game object that plays music created; currently plays Slow Ride. Does not disappear when scene changes.

---
+++ Assets/Music.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Play music
+    private AudioSource musics;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        musics = GetComponent<AudioSource>();
+    }
+}
